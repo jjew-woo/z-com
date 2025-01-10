8e05a4a9d28887c93bf564a121f1a1cef3299538
Jiwoo <jjew0504@naver.com>
2025-01-10 13:30:12 +0900

    add: 로그인 모달 (패러렐 라우트 & 인터셉팅 라우트)

--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
-import styles from "./page.module.css";
-import zLogo from "../../public/zlogo.png";
+import styles from "@/app/page.module.css";
+import zLogo from "../../../public/zlogo.png";
 
 export default function Main() {
   return (
@@ -16,7 +16,7 @@ export default function Main() {
           계정 만들기
         </Link>
         <h3>이미 트위터에 가입하셨나요?</h3>
-        <Link href="/login" className={styles.login}>
+        <Link href="/i/flow/login" className={styles.login}>
           로그인
         </Link>
       </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,9 +17,7 @@ type Props = {
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <div className={styles.container}>{children}</div>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
